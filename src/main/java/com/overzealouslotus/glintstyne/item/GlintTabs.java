94afa597e2c9d9416a3e49439cd863a8c6336a81
OverzealousLotus <OverzealousLotus@proton.me>
2025-03-14 22:09:41 -0700

    Partial Automation work

--- src/main/java/com/overzealouslotus/glintstyne/item/GlintTabs.java
+++ src/main/java/com/overzealouslotus/glintstyne/item/GlintTabs.java
@@ -1,5 +1,6 @@
 package com.overzealouslotus.glintstyne.item;
 
+import com.google.common.collect.ImmutableSet;
 import com.overzealouslotus.glintstyne.Glintstyne;
 import com.overzealouslotus.glintstyne.block.GlintBlocks;
 import net.minecraft.core.registries.Registries;
@@ -10,23 +11,38 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class GlintTabs {
-    public static final DeferredRegister<CreativeModeTab> GLINT_TABS =
-        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Glintstyne.MOD_ID);
+  public static final DeferredRegister<CreativeModeTab> GLINT_TABS =
+    DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Glintstyne.MOD_ID);
+  private static final Supplier<ImmutableSet<ItemStack>> CREATIVE_ITEMS = () -> ImmutableSet.of(
+    GlintItems.RAW_MOCHITE,
+    GlintItems.MOCHITE,
 
-    public static final RegistryObject<CreativeModeTab> GLINTSTYNE_TAB =
-        GLINT_TABS.register("glintstyne_tab", () -> CreativeModeTab.builder()
-            .icon(() -> new ItemStack(GlintItems.MOCHITE.get()))
-            .title(Component.translatable("creativetab.glintstyne_tab"))
-            .displayItems(((itemDisplayParameters, output) -> {
-                output.accept(GlintItems.RAW_MOCHITE.get());
-                output.accept(GlintItems.MOCHITE.get());
-                output.accept(GlintBlocks.MOCHITE_BLOCK.get());
-                output.accept(GlintBlocks.RAW_MOCHITE_BLOCK.get());
-                output.accept(GlintBlocks.MOCHITE_ORE.get());
-            })).build());
+    GlintItems.MORKITE
+  ).stream().map(item -> item.get().getDefaultInstance())
+    .collect(ImmutableSet.toImmutableSet());
+  private static final Supplier<ImmutableSet<ItemStack>> CREATIVE_BLOCKS = () -> ImmutableSet.of(
+    GlintBlocks.MOCHITE_ORE,
+    GlintBlocks.DEEPSLATE_MOCHITE_ORE,
+    GlintBlocks.RAW_MOCHITE_BLOCK,
+    GlintBlocks.MOCHITE_BLOCK,
 
-    public static void register(IEventBus eventBus) {
-        GLINT_TABS.register(eventBus);
-    }
+    GlintBlocks.DEEPSLATE_MORKITE_ORE
+  ).stream().map(block -> block.get().asItem().getDefaultInstance())
+    .collect(ImmutableSet.toImmutableSet());
+
+  public static final RegistryObject<CreativeModeTab> GLINTSTYNE_TAB =
+    GLINT_TABS.register("glintstyne_tab", () -> CreativeModeTab.builder()
+      .icon(() -> new ItemStack(GlintItems.MOCHITE.get()))
+      .title(Component.translatable("creativetab.glintstyne_tab"))
+      .displayItems(((itemDisplayParameters, output) -> {
+        output.acceptAll(CREATIVE_ITEMS.get());
+        output.acceptAll(CREATIVE_BLOCKS.get());
+      })).build());
+
+  public static void register(IEventBus eventBus) {
+    GLINT_TABS.register(eventBus);
+  }
 }
